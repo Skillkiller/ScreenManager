6d5e46561a902129550082af28249bc53dd61d47
Skillkiller <Skillkiller@thecoin.de>
2018-03-10 03:27:00 +0100

    Schnittstelle für Shell erstellt

---
+++ src/main/java/de/skillkiller/servermanager/core/ScreenCommand.java
@@ -0,0 +1,43 @@
+package de.skillkiller.servermanager.core;
+
+import util.ServerObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Skillkiller on 10.03.2018.
+ */
+public class ScreenCommand {
+    public void startCommand(ServerObject serverObject) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.directory(serverObject.getServerDir());
+        String aussen = String.format("screen -dmS %s %s",
+                serverObject.getName(),
+                serverObject.getRestart() ? String.format("su -c \"bash -c 'while true; do %s; echo \\\"Beendet - Restart folgt\\\"; sleep 5; done'\" %s", serverObject.getStartCMD(),
+                        serverObject.getBenutzer()) :
+                String.format("su -c '%s' %s", serverObject.getStartCMD(), serverObject.getBenutzer()));
+
+        System.out.println(aussen);
+    }
+
+    public void stopCommand(ServerObject serverObject) {
+        //TODO Überarbeiten!
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.directory(serverObject.getServerDir());
+        processBuilder.command(String.format("screen -S %s -X %s",
+                serverObject.getName(), serverObject.getStopCMD()));
+        processBuilder.command(String.format("screen -S %s -X quit", serverObject.getName()));
+
+        System.out.println(processBuilder.command());
+        try {
+            Process process = processBuilder.start();
+            new BufferedReader(new InputStreamReader(process.getInputStream())).lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
